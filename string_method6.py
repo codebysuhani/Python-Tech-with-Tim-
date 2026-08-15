1e3cebecbc92609831b5a13da1a6dcc5a811f42b
buildwithsuhani <suhani.online45@gmail.com>
2026-08-15 21:57:49 +0530

    2nd commit

---
+++ string_method6.py
@@ -0,0 +1,14 @@
+#slice operator
+fruits = ["apple" , "pear" , "mango"]
+text = "Hello I like Python"
+
+print(text[1])
+
+#print(text[start:stop:step])
+print(text[0:5:2])
+
+print(fruits[2])
+
+fruits[2:2] = 'grapes'
+
+print(fruits)
